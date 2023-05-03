2c1a87c6cd0e19af3ceb8adb9bd576aa7ccb64d1
KZ <zengke940420@gmail.com>
2023-05-02 22:35:57 -0400

    align sample names.

--- Scripts/CalprotectinLevelBoxPlots.R
+++ Scripts/CalprotectinLevelBoxPlots.R
@@ -8,6 +8,7 @@ calprotectin<-na.omit(calprotectin)
 rownames(calprotectin)<-calprotectin$sample_code
 calprotectin<-calprotectin[, 3, drop = F]
 metaData<-read.delim("metaGvN.txt", sep = "\t", row.names = 1, header = T)
+calprotectin<-calprotectin[rownames(metaData), , drop = F]
 t_test_p<-t.test(unlist(calprotectin)~factor(metaData$dx))$p.value
 wilcox_p<-pvalue(wilcox_test(unlist(calprotectin)~factor(metaData$dx)))
 label = c("GVHD", "No GVHD")
