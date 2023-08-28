dc8c03856896863cd48245eae02d884320254a38
KZ <zengke940420@gmail.com>
2023-08-28 01:13:51 -0400

    stool collection day relative to transplant day.

--- Scripts/StoolAndTransplantDateScatterPlot.R
+++ Scripts/StoolAndTransplantDateScatterPlot.R
@@ -1,22 +1,43 @@
 rm(list= ls())
+library(stringr)
+meta1<-read.delim("metaGvN_index.txt", header = T, row.names = 1)
+meta2<-read.delim("metaSvR_index.txt", header = T, row.names = 1)
 
-myT<-read.csv("DatesOfStoolAndTP.csv", header = T)
+meta1[rownames(meta2), 1]<-meta2$dx
+rownames(meta1)<-sapply(str_split(rownames(meta1), "_", n = 2), `[`, 1)
+rownames(meta1)<-as.numeric(rownames(meta1))
+
+myT<-read.csv("TransplantStoolDate.csv", header = T)
 myT<-na.omit(myT)
 
-myT$Stool.Date<-as.Date(myT$Stool.Date, format="%Y-%m-%d")
-myT$Transplant.date<-as.Date(myT$Transplant.date, format="%Y-%m-%d")
+myT$stool_date<-as.Date(myT$stool_date, format="%Y-%m-%d")
+myT$bmt_date<-as.Date(myT$bmt_date, format="%Y-%m-%d")
 
-myT$days<-as.numeric(myT$Stool.Date - myT$Transplant.date)
+myT$days<-as.numeric(myT$stool_date - myT$bmt_date)
 
-pdf("Plots/StoolTransplantDatePlot.pdf", width = 15, height = 15)
-par(mar=c(6,6,4,1)+.1)
-plot(1, 1, xlim = range(myT$days), 
+myT$condition<-meta1[as.character(myT$code), 1]
+myT$index<-meta1[as.character(myT$code), 2]
+myT<-myT[myT$days >= 0, , drop = F]
+myT<-myT[myT$days < 400, , drop = F]
+
+myT<-myT[order(myT$condition), , drop = F]
+
+pdf("Plots/StoolTransplantDatePlot.pdf", width = 12, height = 15)
+par(mar=c(6,7,4,1)+.1)
+plot(1, 1, xlim = c(0,220), 
      ylim = c(1, nrow(myT)), 
-     type="n", xaxt="n", xlab="", ylab="Sample Code", main = "Stool Collection Date relative to Transplant Date",
+     type="n", xaxt="n", xlab="", ylab="", main = "Stool Collection Date relative to Transplant Date",
      yaxt="n", las=1)
-points(myT$days, 1:nrow(myT), col="cornflowerblue", pch=19)
-axis(2, at=1:nrow(myT), labels=myT$Code, las=1)
-axis(1, at=seq(min(myT$days), max(myT$days), by=50), las = 3)
+color_mapping<-c("no_gvhd" = "cornflowerblue", 
+                 "steroid_refractory_gvhd" = "coral3",
+                 "steroid_sensitive_gvhd" = "tan2")
+
+points(myT$days, 1:nrow(myT), col=color_mapping[myT$condition], pch=19)
+axis(2, at=1:nrow(myT), labels=myT$index, las=1)
+axis(1, at=seq(0, 220, by=5), las = 3)
 mtext("Days", side=1, line=3)
+mtext("Sample Type", side = 2, line = 5)
+legend("topright", legend = names(color_mapping), fill = color_mapping, border = "white", bty="n")
+
 
 dev.off()
